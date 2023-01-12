83a7cd7ca0075fc69110f98939c56a4b8c6914c7
AndreaDeCeglia <deceglia89@gmail.com>
2023-01-12 18:48:05 +0100

    azure bar done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,5 +17,41 @@
             </div>
         </div>
         
-    </div> 
+    </div>
+    
+    <div class="container-icons-blue bg-primary">
+        <div class="icons-blue w-75 m-auto d-flex p-4">
+            <div>
+                <a href="" class="d-flex p-3">
+                    <img src="{{ asset('images/buy-comics-digital-comics.png') }}" alt="">
+                    <h4>DIGITAL COMICS</h4>
+                </a>
+            </div>
+            <div>
+                <a href="" class="d-flex p-3"> 
+                    <img src="{{ asset('images/buy-comics-merchandise.png') }}" alt="">
+                    <h4>DC MERCHANDISE</h4>
+                </a>
+            </div>
+            <div>
+                <a href="" class="d-flex p-3">
+                    <img src="{{ asset('images/buy-comics-subscriptions.png') }}" alt="">
+                    <h4>SUBSCRIPTION</h4>
+                </a>
+            </div>
+            <div>
+                <a href="" class="d-flex p-3">
+                    <img src="{{ asset('images/buy-comics-shop-locator.png') }}" alt="">
+                    <h4>COMIC SHOP LOCATION</h4>
+                </a>
+            </div>
+            <div>
+                <a href="" class="d-flex p-3">
+                    <img src="{{ asset('images/buy-dc-power-visa.svg') }}" alt="">
+                    <h4>DC POWER VISA</h4>
+                </a>
+            </div>
+        </div>
+
+    </div>
 @endsection
