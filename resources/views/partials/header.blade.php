cfc3d8c51eb8533bc2eb964c9d299c075b984dff
AndreaDeCeglia <deceglia89@gmail.com>
2023-01-12 16:56:29 +0100

    header & jumbotron done

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,60 @@
+<nav class="w-100 mx-3">
+    <ul class="d-flex justify-content-around align-items-center list-unstyled w-100">
+        <li>
+            <img src="{{ asset('images/dc-logo.png') }}" alt="">
+        </li>
+        <li>
+            <a href="">
+                prova
+            </a>
+        </li>
+        <li>
+            <a href="/">
+                COMICS
+            </a>
+        </li>
+        <li>
+            <a href="">
+                prova
+            </a>
+        </li>
+        <li>
+            <a href="">
+                prova
+            </a>
+        </li>
+        <li>
+            <a href="">
+                prova
+            </a>
+        </li>
+        <li>
+            <a href="">
+                prova
+            </a>
+        </li>
+        <li>
+            <a href="">
+                prova
+            </a>
+        </li>
+        <li>
+            <a href="">
+                prova
+            </a>
+        </li>
+        <li>
+            <a href="">
+                prova
+            </a>
+        </li>
+        <li>
+            <a href="">
+                prova
+            </a>
+        </li>
+    </ul>
+</nav>
+
+<div class="jumbo-container">
+</div>
